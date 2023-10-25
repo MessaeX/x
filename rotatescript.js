4979cb440245d9718a04ccbd27b722d66683c7ae
MessaeX <148967858+MessaeX@users.noreply.github.com>
2023-10-25 14:02:25 +0300

    Add files via upload

---
+++ rotatescript.js
@@ -0,0 +1,101 @@
+/*This is massive code for rotating of FAQ section*/
+function rotate() {
+  let i=0;
+let button = document.getElementById("accordion-button");
+if (button.style.transform === "rotate(0deg)") {
+
+  button.style.transform = "rotate(45deg)";
+} else if (button.style.transform = "rotate(45deg)") {
+
+  button.style.transform = "rotate(0deg)";
+}
+}
+
+function rotate2() {
+  let i=0;
+let button = document.getElementById("accordion-button2");
+if (button.style.transform === "rotate(0deg)") {
+
+  button.style.transform = "rotate(45deg)";
+} else if (button.style.transform = "rotate(45deg)") {
+
+  button.style.transform = "rotate(0deg)";
+}
+}
+function rotate3() {
+  let i=0;
+let button = document.getElementById("accordion-button3");
+if (button.style.transform === "rotate(0deg)") {
+
+  button.style.transform = "rotate(45deg)";
+} else if (button.style.transform = "rotate(45deg)") {
+
+  button.style.transform = "rotate(0deg)";
+}
+}
+function rotate4() {
+  let i=0;
+let button = document.getElementById("accordion-button4");
+if (button.style.transform === "rotate(0deg)") {
+
+  button.style.transform = "rotate(45deg)";
+} else if (button.style.transform = "rotate(45deg)") {
+
+  button.style.transform = "rotate(0deg)";
+}
+}
+function rotate5() {
+  let i=0;
+let button = document.getElementById("accordion-button5");
+if (button.style.transform === "rotate(0deg)") {
+
+  button.style.transform = "rotate(45deg)";
+} else if (button.style.transform = "rotate(45deg)") {
+
+  button.style.transform = "rotate(0deg)";
+}
+}
+function rotate6() {
+  let i=0;
+let button = document.getElementById("accordion-button6");
+if (button.style.transform === "rotate(0deg)") {
+
+  button.style.transform = "rotate(45deg)";
+} else if (button.style.transform = "rotate(45deg)") {
+
+  button.style.transform = "rotate(0deg)";
+}
+}
+function rotate7() {
+  let i=0;
+let button = document.getElementById("accordion-button7");
+if (button.style.transform === "rotate(0deg)") {
+
+  button.style.transform = "rotate(45deg)";
+} else if (button.style.transform = "rotate(45deg)") {
+
+  button.style.transform = "rotate(0deg)";
+}
+}
+function rotate8() {
+  let i=0;
+let button = document.getElementById("accordion-button8");
+if (button.style.transform === "rotate(0deg)") {
+
+  button.style.transform = "rotate(45deg)";
+} else if (button.style.transform = "rotate(45deg)") {
+
+  button.style.transform = "rotate(0deg)";
+}
+}
+function rotate9() {
+  let i=0;
+let button = document.getElementById("accordion-button9");
+if (button.style.transform === "rotate(0deg)") {
+
+  button.style.transform = "rotate(45deg)";
+} else if (button.style.transform = "rotate(45deg)") {
+
+  button.style.transform = "rotate(0deg)";
+}
+}
